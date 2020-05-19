bf12676ed4837e13d9f1fc85866024a68ddb453a
yambo92 <1810962886@qq.com>
2020-05-19 16:53:49 +0800

    'api'

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import MSite from "../pages/MSite/MSite.vue";
 import Search from "../pages/Search/Search.vue";
 import Order from "../pages/Order/Order.vue";
 import Profile from "../pages/Profile/Profile.vue";
+import Login from "../pages/Login/Login.vue";
 
 //声明使用插件
 Vue.use(VueRouter);
@@ -17,18 +18,34 @@ export default new VueRouter({
     {
       path: "/msite",
       component: MSite,
+      meta: {
+        showFooter: true,
+      },
     },
     {
       path: "/search",
       component: Search,
+      meta: {
+        showFooter: true,
+      },
     },
     {
       path: "/order",
       component: Order,
+      meta: {
+        showFooter: true,
+      },
     },
     {
       path: "/profile",
       component: Profile,
+      meta: {
+        showFooter: true,
+      },
+    },
+    {
+      path: "/login",
+      component: Login,
     },
     {
       path: "/",
